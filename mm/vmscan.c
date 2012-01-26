b42f797767e75bf9c3d0acdac67cd552b09f8b90
Paul Reioux <paul@paul-Precision-WorkStation-T5500.(none)>
2012-01-26 14:57:57 -0800

    mm/vmscan: Add #ifdef around "WasActive page flag"
    also fix a merge error @ putback_lru_page

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -656,8 +656,10 @@ void putback_lru_page(struct page *page)
 	int was_unevictable = PageUnevictable(page);
 
 	VM_BUG_ON(PageLRU(page));
+#ifdef CONFIG_CLEANCACHE
 	if (active)
 		SetPageWasActive(page);
+#endif
 
 redo:
 	ClearPageUnevictable(page);
@@ -1344,7 +1346,9 @@ unsigned long clear_active_flags(struct list_head *page_list,
 		if (PageActive(page)) {
 			lru += LRU_ACTIVE;
 			ClearPageActive(page);
+#ifdef CONFIG_CLEANCACHE
 			SetPageWasActive(page);
+#endif
 			nr_active += numpages;
 		}
 		if (count)
@@ -1773,7 +1777,9 @@ static void shrink_active_list(unsigned long nr_pages, struct zone *zone,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
+#ifdef CONFIG_CLEANCACHE
 		SetPageWasActive(page);
+#endif
 		list_add(&page->lru, &l_inactive);
 	}
 

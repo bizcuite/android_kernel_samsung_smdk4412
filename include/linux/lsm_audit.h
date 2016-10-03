46ffd3b5489635d3d08b8f38270a92464fc4a5bf
bizcuite <bizcuite@gmail.com>
2016-10-03 08:21:30 +0200

    fix:security: add ioctl specific auditing to lsm_audit-already added in this samsung repo

--- include/linux/lsm_audit.h
+++ include/linux/lsm_audit.h
@@ -29,11 +29,6 @@ struct lsm_ioctlop_audit {
 	u16 cmd;
 };
 
-struct lsm_ioctlop_audit {
-	struct path path;
-	u16 cmd;
-};
-
 /* Auxiliary data to use in generating the audit record. */
 struct common_audit_data {
 	char type;
@@ -48,6 +43,7 @@ struct common_audit_data {
 #define LSM_AUDIT_DATA_INODE	9
 #define LSM_AUDIT_DATA_DENTRY	10
 #define LSM_AUDIT_DATA_IOCTL_OP	11
+struct task_struct *tsk;
 	union 	{
 		struct path path;
 		struct dentry *dentry;
